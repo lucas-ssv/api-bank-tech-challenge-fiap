ca426b54ecb328c37a0ad8a1de374725d9db74e3
Lucas Vieira <lucas_99silva@hotmail.com>
2025-01-20 16:22:50 -0300

    feat: add documents upload controller

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 require('dotenv').config()
 const express = require('express')
-const app = express()
-const PORT = process.env.PORT
+const multer = require('multer')
+const cors = require('cors')
 
-app.get('/', (req, res) => {
-  return res.send('Hello World!!')
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    const [, ext] = file.originalname.split('.')
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, `${uniqueSuffix}.${ext}`)
+  }
+})
+const upload = multer({ storage })
+
+const PORT = process.env.PORT
+const app = express()
+app.use(cors())
+app.use("/uploads", express.static("./uploads"));
+app.use(express.json())
+
+app.post('/transaction/documents/upload', upload.array('documents', 3), (req, res) => {
+  const files = req.files
+
+  const filesList = files.map((file) => {
+    return `http://localhost:3004/uploads/${file.filename}`
+  })
+  return res.json(filesList)
 })
 
 app.listen(PORT, () => {
